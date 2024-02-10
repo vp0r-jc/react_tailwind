01d1db3452ee153394cf1131ce2e89110acf5a0a
jcvp0r <juan.hspx@gmail.com>
2024-02-10 02:06:56 -0600

    ya se puede cambiar el contenido del main usando un id que viene del content.js (falta ver como mover el id desde el SideMenu)

---
+++ pracReact/src/assets/content.js
@@ -0,0 +1,12 @@
+export const content = [
+    {
+        id: 0,
+        title: "Todo sobre el Blockchain",
+        cont: "¿Que es la tecnologia Blochain?"
+    },
+    {
+        id:1,
+        title: "¿Que es el Bitcoin?",
+        cont: "Contenido sobre Bitcoin"
+    }
+]
--- pracReact/src/components/SideMenu.jsx
+++ pracReact/src/components/SideMenu.jsx
@@ -2,10 +2,10 @@
 function SideMenu() {
   return (
     <div className="bg-[#303134] grid self-start overflow-hidden rounded-md border border-[#D7DAE9] font-mono">
-      <div className="flex justify-center bg-[#D7DAE9]">
+      <div className="flex justify-center bg-[#D7DAE9] ">
         <h1>Title</h1>
       </div>
-      <div className="text-white text-[12px] p-2">
+      <div className="text-white text-[12px] p-2 ">
         <p className="">Welcome to proyect <span className="text-lg
          font-bold">Schema</span>, a place where you can find information about the future of the next financial system, cryptocurrencies and bockchain.</p>
       </div>
